c7c17ffefe5bedc59739fb289c502f8529379902
Alex Fryzowicz <aleksandra.fryzowicz@gmail.com>
2020-07-09 16:27:24 +0200

    MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,15 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String textType = "first text type";
+        int year = 2020;
+        double yearMonth = 07.2020;
+        boolean magicWorld = true;
+        char znak = 'A';
+        System.out.println(textType + " " + year + " " + yearMonth);
+        System.out.println(textType);
+        System.out.println(yearMonth);
+        System.out.println(year);
+        System.out.println(magicWorld);
+    }
+
+}
