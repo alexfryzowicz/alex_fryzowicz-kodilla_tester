b224190be48d2ccb7f9e5d361befc20051d223dc
Alex Fryzowicz <aleksandra.fryzowicz@gmail.com>
2020-07-18 13:40:23 +0200

    notebook

---
+++ kodilla-intro/src/main/java/Notebook.java
@@ -0,0 +1,44 @@
+public class Notebook {
+    int weight;
+    int price;
+    int year;
+
+    public Notebook(int weight, int price, int year) {
+        this.weight = weight;
+        this.price = price;
+        this.year = year;
+    }
+    public void checkPrice() {
+        if (this.price < 600) {
+            System.out.println("this notebook is very cheap");
+        }
+        else if (this.price < 600 && this.price > 1000){
+            System.out.println("the price is good");
+        }
+        else {
+            System.out.println("this notebook is expensive");
+        }
+    }
+    public void checkWeight() {
+        if (this.weight < 600) {
+            System.out.println("this notebook is light");
+        }
+        else if (this.weight > 600 && this.weight < 2000){
+            System.out.println("this notebook is heavy");
+        }
+        else {
+            System.out.println("this notebook is very heavy");
+        }
+    }
+    public void checkBest() {
+        if (this.weight < 600 && this.year < 1992) {
+            System.out.println("this notebook of the ligest of the 1992'");
+        }
+        else if (this.weight > 600 && this.year < 1992){
+            System.out.println("this notebook of the ligest of the 1992'");
+        }
+        else if (this.weight > 12000 && this.year == 2010){
+            System.out.println("this notebook is the latest and the heaviest in 2010'");
+        }
+    }
+}
