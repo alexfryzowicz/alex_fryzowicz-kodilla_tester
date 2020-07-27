82e88efa0350bf607596aeb87718603a0605cad6
Alex Fryzowicz <aleksandra.fryzowicz@gmail.com>
2020-07-27 22:24:15 +0200

    User

--- kodilla-intro/src/main/java/User.java
+++ kodilla-intro/src/main/java/User.java
@@ -1,8 +1,45 @@
+import java.util.Random;
+
 public class User {
-    public static void main(String[] args) {
 
         String name;
         int age;
 
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
+
+    public String showName() {
+        System.out.println(this.name);
+        return this.name;
+    }
+
+    public int showAge() {
+        System.out.println(this.age);
+        return this.age;
+    }
+
+    public static void main(String[] args) {
+        User Bill = new User ("Bill", 17);
+        User Jack = new User ("Jack", 22);
+        User Jim = new User ("Jim", 29);
+        User Mary = new User ("Mary", 5);
+
+        User[] users = new User[] {Bill, Jack, Jim, Mary};
+
+
+    int sum = 0;
+    for (int i = 0; i < users.length; i++) {
+        sum = sum + users[i].showAge();
+    }
+    int average = sum / users.length;
+    for (int i = 0; i < users.length; i++) {
+        User user = users[i];
+        int age= user.showAge();
+        if (age > average) {
+            System.out.println(user.showName());
+        };
+    }
+}
 }
---
+++ kodilla-intro/src/main/java/UserRunner.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class UserRunner {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int result = 0;
+        int sum = 0;
+        int minimum = 0;
+        int maximum = 30;
+
+        while (sum < 5000) {
+            int temp = random.nextInt(31);
+            sum = sum + temp;
+            System.out.println(temp);
+            result ++;
+            for (temp = 0; temp < minimum; temp++);
+            minimum = temp;
+            System.out.println("the lowest number is " + temp);
+            for (temp = 30; temp > maximum; temp++);
+            maximum = temp;
+            System.out.println("the highest number is " + temp);
+        }
+} }
