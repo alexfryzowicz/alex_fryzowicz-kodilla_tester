3a91268a940d533f67a10be31fb053795bf11c8c
Alex Fryzowicz <aleksandra.fryzowicz@gmail.com>
2020-08-26 11:43:13 +0200

    task

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskApp.java
@@ -0,0 +1,10 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class TaskApp {
+    public static void main(String[] args) {
+        Task task = new TaskHacked("task", 6);
+        TaskHacked taskHacked = (TaskHacked) task;
+        taskHacked.modifyTitle("new task");
+        System.out.println(task.getTitle());
+    }
+}
---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskHacked.java
@@ -0,0 +1,14 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class TaskHacked extends Task {
+    private String realTitle;
+
+    public TaskHacked(String title, int duration) {
+        super(title, duration);
+    }
+
+    public void modifyTitle(String newTitle) {
+        title = newTitle;
+    }
+
+}
