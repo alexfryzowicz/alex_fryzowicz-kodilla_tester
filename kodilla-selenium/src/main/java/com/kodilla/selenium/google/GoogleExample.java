e3406f0f5997a0ea21b9d07f69ca61b6beda29ff
Alex Fryzowicz <aleksandra.fryzowicz@gmail.com>
2020-09-27 18:23:58 +0200

    test

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/google/GoogleExample.java
@@ -0,0 +1,28 @@
+package com.kodilla.selenium.google;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GoogleExample {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\Chrome\\chromedriver.exe");
+        WebDriver driver1 = new ChromeDriver();
+        driver1.get("https://www.google.com");
+
+        WebElement inputField1 = driver1.findElement(By.name("q"));
+        inputField1.sendKeys("Kodilla");
+        inputField1.submit();
+
+        System.setProperty("webdriver.gecko.driver", "C:\\selenium-drivers\\Firefox\\geckodriver.exe");
+        WebDriver driver2 = new FirefoxDriver();
+        driver2.get("https://www.google.com");
+
+        WebElement inputField2 = driver2.findElement(By.name("q"));
+        inputField2.sendKeys("kodilla");
+        inputField2.submit();
+
+    }
+}
